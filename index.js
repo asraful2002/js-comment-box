03df22d9036116f3dc6660df0f60aacd20f8f67c
Asraful Islam <asraful2002@example.com>
2026-04-30 23:31:08 -0700

    hellow

--- index.js
+++ index.js
@@ -1,40 +1,4 @@
 
-// const btn = document.getElementById("addBtn");
-// const input = document.getElementById("commentInput");
-// const list = document.getElementById("commentList");
-
-
-
-// btn.addEventListener("click", function () {
-//  let text = input.value;
-
-//  if(text === ""){
-//   return;
-//  }
-
-//  if(Number(text)){
-//   alert("Numbers are not allowed!")
-//   return;
-//  }
-
-// let words = text.split(" ");
-//  if (words.length > 10) { 
-//   alert("Maximum 10 words allowed!"); return; }
-
-//  const items = list.getElementsByTagName("li");
-//  for(let i=0; i<items.length;i++){
-//   if(items[i].innerText === text){
-//     alert("This Comment Already Add")
-//     return;
-//   }
-//  }
-
-//  const li = document.createElement("li");
-//  li.innerText = text;
-//  list.appendChild(li);
-//  input.value = "";
-// });
-
 
 
 const btn = document.getElementById("addBtn");
@@ -84,7 +48,7 @@ btn.addEventListener("click", function () {
 
   // UI show (ID আগে, text পরে)
   const li = document.createElement("li");
- li.innerText = id + " (" + text + ")";
+ li.innerText = id + " (" + text +")";
   list.appendChild(li);
 
   input.value = "";

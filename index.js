f3d6668ccf610dcfae750b3b2f739b20f02008d4
Asraful Islam <asraful2002@example.com>
2026-04-30 23:17:11 -0700

    update

--- index.js
+++ index.js
@@ -1,36 +1,91 @@
 
+// const btn = document.getElementById("addBtn");
+// const input = document.getElementById("commentInput");
+// const list = document.getElementById("commentList");
+
+
+
+// btn.addEventListener("click", function () {
+//  let text = input.value;
+
+//  if(text === ""){
+//   return;
+//  }
+
+//  if(Number(text)){
+//   alert("Numbers are not allowed!")
+//   return;
+//  }
+
+// let words = text.split(" ");
+//  if (words.length > 10) { 
+//   alert("Maximum 10 words allowed!"); return; }
+
+//  const items = list.getElementsByTagName("li");
+//  for(let i=0; i<items.length;i++){
+//   if(items[i].innerText === text){
+//     alert("This Comment Already Add")
+//     return;
+//   }
+//  }
+
+//  const li = document.createElement("li");
+//  li.innerText = text;
+//  list.appendChild(li);
+//  input.value = "";
+// });
+
+
+
 const btn = document.getElementById("addBtn");
 const input = document.getElementById("commentInput");
 const list = document.getElementById("commentList");
 
-
+let comments = [];
 
 btn.addEventListener("click", function () {
- let text = input.value;
+  let text = input.value;
 
- if(text === ""){
-  return;
- }
-
- if(Number(text)){
-  alert("Numbers are not allowed!")
-  return;
- }
-
-let words = text.split(" ");
- if (words.length > 10) { 
-  alert("Maximum 10 words allowed!"); return; }
-
- const items = list.getElementsByTagName("li");
- for(let i=0; i<items.length;i++){
-  if(items[i].innerText === text){
-    alert("This Comment Already Add")
+  if (text === "") {
     return;
   }
- }
 
- const li = document.createElement("li");
- li.innerText = text;
- list.appendChild(li);
- input.value = "";
+  if (!isNaN(text)) {
+    alert("number not allow");
+    return;
+  }
+
+  let words = text.split(" ");
+  if (words.length > 10) {
+    alert("Maximum 10 word");
+    return;
+  }
+
+  const items = list.getElementsByTagName("li");
+  for (let i = 0; i < items.length; i++) {
+    if (items[i].innerText === text) {
+      alert("This Comment Already Add");
+      return;
+    }
+  }
+
+  // ID generate
+  const id = Math.random().toString(36).substring(2, 9);
+
+  // object create
+  const commentObj = {
+    id: id,
+    text: text
+  };
+
+  comments.push(commentObj);
+
+  console.log(comments);
+
+  // UI show (ID আগে, text পরে)
+  const li = document.createElement("li");
+ li.innerText = id + " (" + text + ")";
+  list.appendChild(li);
+
+  input.value = "";
 });
